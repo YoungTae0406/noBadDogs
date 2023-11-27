187a3cc87bcf1af306945e8a343d05723e028bf0
202202574 <maine0948@naver.com>
2023-11-27 18:23:08 +0900

    feat : Add review.dart, walkman.dart

---
+++ lib/review.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'MainPage.dart';
+
+void main() {
+  runApp(Review());
+}
+
+class Review extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: null,
+        body: Stack(
+          children: [
+            Center(
+              child: Image.asset(
+                'Image/review.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              bottom: 200,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPage()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
+                  ),
+                  child: Text('완료'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
